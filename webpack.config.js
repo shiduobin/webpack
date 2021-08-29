02fd04987ccf6190db0f55ca68c8da034a52668d
shiduobin <shiduobin123@163.com>
2021-08-29 22:41:53 +0800

    feat:  添加动态多页面打包

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,49 @@
  * @Author: shiduobin
  * @Date: 2021-07-25 17:52:17
  * @LastEditors: shiduobin
- * @LastEditTime: 2021-08-11 23:09:42
+ * @LastEditTime: 2021-08-29 22:35:53
  */
 
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const glob = require("glob");
+
+const setMPA = () => {
+  const entry = {};
+  const htmlwebpackplugins = [];
+  const entryFiles = glob.sync(path.join(__dirname, "./src/*/index.js"));
+  console.log(entryFiles);
+  entryFiles.map((item, index) => {
+    const entryFile = entryFiles[index];
+    const match = entryFile.match(/src\/(.*)\/index\.js$/);
+    const pageName = match && match[1];
+    console.log(pageName);
+    entry[pageName] = entryFile;
+    htmlwebpackplugins.push(
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname,`./src/${pageName}/index.html`),
+        filename: `${pageName}.html`,
+        chunks: [pageName],
+      })
+    );
+  });
+  return {
+    entry,
+    htmlwebpackplugins,
+  };
+};
+
+const { entry, htmlwebpackplugins } = setMPA();
 module.exports = {
   // 入口文件
-  entry: ["./src/index.js"],
+  // entry: ["./src/index.js"],
+  // entry: {
+  //   index: "./src/index.js",
+  //   list: "./src/list.js",
+  //   detail: "./src/detail.js",
+  // },
+  entry,
   // 出口文件
   output: {
     // 打包后文件的输出路径
@@ -51,7 +85,7 @@ module.exports = {
             name: "[name]_[hash:5].[ext]",
             limit: 5000, // fonts file size <= 5KB, use 'base64'; else, output file
             publicPath: "fonts/",
-            outputPath: "fonts/"
+            outputPath: "fonts/",
           },
         },
       },
@@ -59,9 +93,21 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: "./index.html",
-      filename: "index.html",
-    }),
+    // new HtmlWebpackPlugin({
+    //   template: "./index.html",
+    //   filename: "index.html",
+    //   chunks: ["index"],
+    // }),
+    // new HtmlWebpackPlugin({
+    //   template: "./index.html",
+    //   filename: "list.html",
+    //   chunks: ["list"],
+    // }),
+    // new HtmlWebpackPlugin({
+    //   template: "./index.html",
+    //   filename: "detail.html",
+    //   chunks: ["detail"],
+    // }),
+    ...htmlwebpackplugins
   ],
 };
